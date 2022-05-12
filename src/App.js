59e41906210b9251dd484aa0c5e8408e3591cf7e
Muhammad Gunawan <gunawandr7@gmail.com>
2022-05-12 14:19:41 +0700

    [INIT] add first react project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const headerTag = "Enigma Camp React"
+  const myStyle = {color : 'black', fontSize : '40px'}
+
+  const [fname, setFname] = useState("")
+
+  const handleChange = e => {
+    setFname(e.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* {embeded expretion} */}
+        <h1 style={myStyle}>{headerTag}</h1>
+        <form>
+        <label style={{fontWeight: 'bold'}}>
+          First Name:{" "}
+          <input type="text" value={fname} onChange={handleChange} />
+        </label>
+      </form>
+      <h5>First name: {fname}</h5>
       </header>
     </div>
   );
