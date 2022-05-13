a5925c8aa50473f355c48f27ca02a4d474866c36
Muhammad Gunawan <gunawandr7@gmail.com>
2022-05-13 16:06:46 +0700

    [IMPROVE] split into componen class

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
+import NavBar from './NavBar';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const headerTag = "Enigma Camp React"
-  const myStyle = {color : 'black', fontSize : '40px'}
-
-  const [fname, setFname] = useState("")
-
-  const handleChange = e => {
-    setFname(e.target.value)
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* {embeded expretion} */}
-        <h1 style={myStyle}>{headerTag}</h1>
-        <form>
-        <label style={{fontWeight: 'bold'}}>
-          First Name:{" "}
-          <input type="text" value={fname} onChange={handleChange} />
-        </label>
-      </form>
-      <h5>First name: {fname}</h5>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <Content/>
+      <Footer/>
+    </>
   );
 }
 
