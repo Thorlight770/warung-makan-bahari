d2058838dbae4fd22695a296df4e5224dcac2cd6
Muhammad Gunawan <gunawandr7@gmail.com>
2022-05-23 13:22:51 +0700

    [IMPROVE] able context

--- src/App.js
+++ src/App.js
@@ -1,94 +1,42 @@
-import React, { Component } from "react";
-import Login from "./login/Login";
-import Menu from "./menu/Menu";
-import 'aos/dist/aos.css';
-import Swal from "sweetalert2";
+import React, { Component, createContext } from 'react'
+import ButtonCounter from './ButtonCounter';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foods: [],
-      tables: [],
-      isLogin: false,
-    };
+
+export const RootContext = createContext();
+const Provider = RootContext.Provider;
+
+class App extends Component {
+  state = {
+    globalNumber : 3
   }
 
-  loginAuth = (auth) => {
-    this.setState({
-      isLogin: auth,
-    });
-  };
+  dispatch = (action) => {
+    if(action.type === "PLUS"){
+      return this.setState({
+        globalNumber : this.state.globalNumber + 1
+      })
+    }
 
-  addFood = (food) => {
-    console.log(food.name);
-    const list = [...this.state.foods];
-    list.push(food);
-    this.setState({
-      foods: list,
-    });
-    Swal.fire(
-      'Success!',
-      'Insert data food!',
-      'success'
-    )
-  };
-
-  removeFood = (index) => {
-    const list = [...this.state.foods];
-    list.splice(index, 1)
-    this.setState({
-      foods : list
-    })
-    Swal.fire(
-      'Success!',
-      'Remove data food!',
-      'success'
-    )
-  }
-
-  addTable = (table) => {
-    console.log(table.status);
-    const list = [...this.state.tables];
-    list.push(table);
-    this.setState({
-      tables: list,
-    });
-    Swal.fire(
-      'Success!',
-      'Insert data table!',
-      'success'
-    )
-  };
-
-  removeTable = (index) => {
-    const list = [...this.state.tables];
-    list.splice(index, 1)
-    this.setState({
-      tables : list
-    })
-    Swal.fire(
-      'Success!',
-      'Remove data food!',
-      'success'
-    )
+    if(action.type === "MINUS"){
+      return this.setState({
+        globalNumber : this.state.globalNumber - 1
+      })
+    }
   }
 
   render() {
-    if (this.state.isLogin) {
-      return (
-        <Menu
-          logout={this.loginAuth}
-          foods={this.state.foods}
-          tables={this.state.tables}
-          addFood={this.addFood}
-          removeFood={this.removeFood}
-          addTable={this.addTable}
-          removeTable={this.removeTable}
-        />
-      );
-    } else {
-      return <Login login={this.loginAuth} />;
-    }
+    return (
+      <Provider value={{
+        state : this.state,
+        dispatch : this.dispatch
+      }}>
+      <div className='text-center'>
+        <h1>{this.state.globalNumber}</h1>
+        <ButtonCounter />
+      </div>
+      </Provider>
+    )
   }
 }
+
+export default App
